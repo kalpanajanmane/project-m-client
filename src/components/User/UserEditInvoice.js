3cc7af8e199edea12bfae39d19d98fa919967089
Saikumar guduru <skg.13.edu@gmail.com>
2024-03-05 22:25:28 +0530

    fix navbar import error

--- src/components/User/UserEditInvoice.js
+++ src/components/User/UserEditInvoice.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./UserEditInvoice.css";
+import UserNavbar from './UserNavbar';
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import A from '../images/v.png';

1672e59da38ec1ca2ef4ed4498dd2c911dda77a2
AakashHadli <aakashhadli@gmail.com>
2024-03-09 16:19:09 +0530

    invoiceManage css

--- src/components/User/UserInvoiceManage.js
+++ src/components/User/UserInvoiceManage.js
@@ -206,21 +206,25 @@ function UserInvoiceManagement() {
 			<div className='invoice-management'>
 				<div className='invoice-management-data'>
 					<div className='invoice-management-data-header'>
-						All Invoice
-						<input
-							type='text'
-							placeholder='Search Invoice Vehicle Number...'
-							className='invoice-manage-search-input'
-							value={searchInput}
-							onChange={(e) => setSearchInput(e.target.value)}
-						/>
-						<label className='date-label-inv'>Date : </label>
-						<input
-							className='date-select-inv'
-							type='date'
-							value={startDate}
-							onChange={handleFromDateSelect}
-						/>
+						<div>
+							All Invoice
+							<input
+								type='text'
+								placeholder='Search Invoice Vehicle Number...'
+								className='invoice-manage-search-input'
+								value={searchInput}
+								onChange={(e) => setSearchInput(e.target.value)}
+							/>
+						</div>
+						<div>
+							<label className='date-label-inv'>Date : </label>
+							<input
+								className='date-select-inv'
+								type='date'
+								value={startDate}
+								onChange={handleFromDateSelect}
+							/>
+						</div>
 					</div>
 					<div className='invoice-management-data-body'>
 						<table className='invoice-management-data-body-table'>
